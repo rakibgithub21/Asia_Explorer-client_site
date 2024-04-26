d38f4862e83053ad8f1bfd0239496d9d73646d9c
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-04-26 11:51:11 +0600

    working on nav var

---
+++ src/shared/MainLayout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default MainLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,9 @@ export default {
   },
   // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
+  
+  daisyui: {
+    themes: ["light", "dark"],
+  },
 }
 
