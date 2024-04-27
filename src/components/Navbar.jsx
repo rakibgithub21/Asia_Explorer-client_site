4251ac5b4d67e75f0024627ce24241db478d2d84
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-04-27 22:14:28 +0600

    use z index value

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,14 +41,14 @@ const Navbar = () => {
 
 
     return (
-        <div className="bg-base-200">
+        <div className="bg-base-200 ">
             <div className="navbar bg-base-200 container mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu z-50 menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                             <NavLink className={({ isActive }) => isActive ? ' text-lg font-medium text-[#0aa9be] underline rounded' : 'text-lg hover:text-rose-500 '} to={'/'}>Home</NavLink>
                             <NavLink className={({ isActive }) => isActive ? ' text-lg font-medium text-[#0aa9be] underline rounded' : 'text-lg hover:text-rose-500 '} to={'/tourist-spot'}>Tourist Spot</NavLink>
                             <NavLink className={({ isActive }) => isActive ? ' text-lg font-medium text-[#0aa9be] underline rounded' : 'hover:text-rose-500 text-lg '} to={'/add-tourist'}>Add Tourist</NavLink>
@@ -82,7 +82,7 @@ const Navbar = () => {
                                         <img title={user?.displayName ? user.displayName : 'Unknown'} alt="Tailwind CSS Navbar component" src={user?.photoURL} />
                                     </div>
                                 </div>
-                                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-2 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                <ul tabIndex={0} className="menu z-50 menu-sm dropdown-content mt-2 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                                     <li><button className="btn btn-outline btn-error" onClick={logoutUser}>Logout</button></li>
                                 </ul>
                             </div>
